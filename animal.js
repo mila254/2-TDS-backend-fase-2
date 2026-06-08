1a20bda187dfad6af8be0d70b959a043077ddef1
mila254 <camila.serna@escola.pr.gov.br>
2026-06-08 13:49:17 +0000

    V1

--- animal.js
+++ animal.js
@@ -1,1 +1,15 @@
+const animal = {
+    nome: "tom" , //string
+    especie: "Cachorro",
+    idade: 2, //number
+    tutor: false,
+    vacinado: true
+};
 
+console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 2;
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com a idade ${animal.idade} e possui sua vacina em dia(${animal.vacinado}).`);
